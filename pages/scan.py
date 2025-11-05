e6b41cb45b92a59ca5adbb21a617e4a3c5db3b72
TheOnlySD12 <helem.gyphon@gmail.com>
2025-11-05 14:45:15 +0200

    t1

--- pages/scan.py
+++ pages/scan.py
@@ -3,16 +3,19 @@ from camera_input_live import camera_input_live
 
 st.title("Scan")
 
-image = camera_input_live()
+image = camera_input_live(debounce=0, height=300, width=300, show_controls=False)
 
 if image:
   st.image(image)
 
-if st.button("Back"):
-    st.switch_page("main.py")
-
+"""
 enable = st.checkbox("Enable camera")
 picture = st.camera_input("Take a picture", disabled=not enable)
 
 if picture:
     st.image(picture)
+
+"""
+
+if st.button("Back"):
+    st.switch_page("main.py")
