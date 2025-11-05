6811c08296f1081722a6e41856ba08b9c8f53673
TheOnlySD12 <helem.gyphon@gmail.com>
2025-11-05 14:47:48 +0200

    t2

--- pages/scan.py
+++ pages/scan.py
@@ -8,14 +8,5 @@ image = camera_input_live(debounce=0, height=300, width=300, show_controls=False
 if image:
   st.image(image)
 
-"""
-enable = st.checkbox("Enable camera")
-picture = st.camera_input("Take a picture", disabled=not enable)
-
-if picture:
-    st.image(picture)
-
-"""
-
 if st.button("Back"):
     st.switch_page("main.py")
