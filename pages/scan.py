556aa114755209fb4d6dc50d3062696bc21567f2
TheOnlySD12 <helem.gyphon@gmail.com>
2025-11-05 14:53:49 +0200

    t3

--- pages/scan.py
+++ pages/scan.py
@@ -3,10 +3,14 @@ from camera_input_live import camera_input_live
 
 st.title("Scan")
 
-image = camera_input_live(debounce=0, height=300, width=300, show_controls=False)
+#image = camera_input_live(debounce=0, height=300, width=300, show_controls=False)
 
+#if image:
+#  st.image(image)
+
+image = st.camera_input()
 if image:
-  st.image(image)
+    st.image(image)
 
 if st.button("Back"):
     st.switch_page("main.py")
