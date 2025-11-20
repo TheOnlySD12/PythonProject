df573b3da0db7ccd112ef86d9e47747dcd5361c8
TheOnlySD12 <helem.gyphon@gmail.com>
2025-11-20 15:20:28 +0200

    mult si bine

--- pages/scan.py
+++ pages/scan.py
@@ -1,19 +1,19 @@
 import streamlit as st
-from streamlit_webrtc import webrtc_streamer
+from streamlit_webrtc import WebRtcMode, webrtc_streamer
 import cv2
+import queue
 
 st.title("Scan")
 
-lastElev = ""
+qr_code = queue.Queue()
 
+last = None
 
 def check(text):
-    global lastElev
-    if lastElev != text:
-        print(text)
-        st.info(text)
-        lastElev = text
-
+    global last
+    if last != text:
+        qr_code.put(text)
+        last = text
 
 def video_frame_callback(frame):
     img = frame.to_ndarray(format="bgr24")
@@ -21,19 +21,29 @@ def video_frame_callback(frame):
     info, bbox, _ = cv2.QRCodeDetector().detectAndDecode(img)
 
     if len(info) > 0:
-        check(info)
+       check(info)
 
     return frame
 
 
-webrtc_streamer(
+webrtc_ctx = webrtc_streamer(
     key="scan",
+    mode=WebRtcMode.SENDRECV,
     video_frame_callback=video_frame_callback,
     media_stream_constraints={
         "video": True,
         "audio": False
-    }
+    },
+    async_processing=True
 )
+
+if st.checkbox("Scaneaza", value=True):
+    if webrtc_ctx.state.playing:
+        labels_placeholder = st.empty()
+        while True:
+            result = qr_code.get()
+            labels_placeholder.write(result)
 
 if st.button("Back"):
     st.switch_page("main.py")
+
